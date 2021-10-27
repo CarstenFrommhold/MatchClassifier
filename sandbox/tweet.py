c5a05efa9538514c5324a54f0af35fff8faec42c
Carsten <carsten.frommhold@datadrivers.de>
2021-10-27 20:21:53 +0200

    twitter sandbox

---
+++ sandbox/tweet.py
@@ -0,0 +1,18 @@
+""" Twitter API
+"""
+import tweepy
+from credentials import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+if __name__ == "__main__":
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    api.update_status("Das Pferd frisst keinen Gurkensalat. #isso")
--- training/data_prep/process_data.py
+++ training/data_prep/process_data.py
@@ -4,14 +4,6 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    """
-    Ideen Feature Engineering
-    Einfache KPI, zB Durchschnittliche Tore via HappyLearning lernen lassen
-    Durchschnittliche Punkte der letzen 3,4,5 Spiele -> "Lauf haben". Cold Start, aber o.k.. 
-    Alternativ: Letzte Saison übernehmen
-    Stammdaten
-    """
-
     seasons = [1011, 1112, 1213, 1314, 1415, 1516, 1617, 1718, 1819, 2021]
     for season in seasons:
         print(f"Preparing data for season {season}")
